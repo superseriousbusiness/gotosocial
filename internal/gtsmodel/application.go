2ebc0101ea97e053e16f0c73fe36cfb1ec2f9c11
tsmethurst <tobi.smethurst@klarrio.com>
2021-03-22 17:01:46 +0100

    add comments

--- internal/gtsmodel/application.go
+++ internal/gtsmodel/application.go
@@ -18,13 +18,38 @@
 
 package gtsmodel
 
+import "github.com/gotosocial/gotosocial/pkg/mastotypes"
+
+// Application represents an application that can perform actions on behalf of a user.
+// It is used to authorize tokens etc, and is associated with an oauth client id in the database.
 type Application struct {
-	ID           string `pg:"type:uuid,default:gen_random_uuid(),pk,notnull"`
-	Name         string
-	Website      string
-	RedirectURI  string `json:"redirect_uri"`
-	ClientID     string `json:"client_id"`
-	ClientSecret string `json:"client_secret"`
-	Scopes       string `json:"scopes"`
-	VapidKey     string `json:"vapid_key"`
+	// id of this application in the db
+	ID string `pg:"type:uuid,default:gen_random_uuid(),pk,notnull"`
+	// name of the application given when it was created (eg., 'tusky')
+	Name string
+	// website for the application given when it was created (eg., 'https://tusky.app')
+	Website string
+	// redirect uri requested by the application for oauth2 flow
+	RedirectURI string
+	// id of the associated oauth client entity in the db
+	ClientID string
+	// secret of the associated oauth client entity in the db
+	ClientSecret string
+   // scopes requested when this app was created
+	Scopes       string
+   // a vapid key generated for this app when it was created
+	VapidKey     string
+}
+
+// ToMastotype returns this application as a mastodon api type, ready for serialization
+func (a *Application) ToMastotype() *mastotypes.Application {
+	return &mastotypes.Application{
+		ID:           a.ID,
+		Name:         a.Name,
+		Website:      a.Website,
+		RedirectURI:  a.RedirectURI,
+		ClientID:     a.ClientID,
+		ClientSecret: a.ClientSecret,
+		VapidKey:     a.VapidKey,
+	}
 }
