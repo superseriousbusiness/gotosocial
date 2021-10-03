e71f3233625c6b0b43e0edb000e04d3928172151
tsmethurst <tobi.smethurst@klarrio.com>
2021-10-03 18:13:01 +0200

    test get followers/following

---
+++ internal/federation/federatingdb/followers_test.go
@@ -0,0 +1,53 @@
+/*
+   GoToSocial
+   Copyright (C) 2021 GoToSocial Authors admin@gotosocial.org
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package federatingdb_test
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/go-fed/activity/streams"
+	"github.com/stretchr/testify/suite"
+	"github.com/superseriousbusiness/gotosocial/testrig"
+)
+
+type FollowersTestSuite struct {
+	FederatingDBTestSuite
+}
+
+func (suite *FollowersTestSuite) TestGetFollowers() {
+	testAccount := suite.testAccounts["local_account_2"]
+
+	f, err := suite.federatingDB.Followers(context.Background(), testrig.URLMustParse(testAccount.URI))
+	suite.NoError(err)
+
+	fi, err := streams.Serialize(f)
+	suite.NoError(err)
+
+	fJson, err := json.Marshal(fi)
+	suite.NoError(err)
+
+	// zork follows local_account_2 so this should be reflected in the response
+	suite.Equal(`{"@context":"https://www.w3.org/ns/activitystreams","items":"http://localhost:8080/users/the_mighty_zork","type":"Collection"}`, string(fJson))
+}
+
+func TestFollowersTestSuite(t *testing.T) {
+	suite.Run(t, &FollowersTestSuite{})
+}
---
+++ internal/federation/federatingdb/following_test.go
@@ -0,0 +1,53 @@
+/*
+   GoToSocial
+   Copyright (C) 2021 GoToSocial Authors admin@gotosocial.org
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package federatingdb_test
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/go-fed/activity/streams"
+	"github.com/stretchr/testify/suite"
+	"github.com/superseriousbusiness/gotosocial/testrig"
+)
+
+type FollowingTestSuite struct {
+	FederatingDBTestSuite
+}
+
+func (suite *FollowingTestSuite) TestGetFollowing() {
+	testAccount := suite.testAccounts["local_account_1"]
+
+	f, err := suite.federatingDB.Following(context.Background(), testrig.URLMustParse(testAccount.URI))
+	suite.NoError(err)
+
+	fi, err := streams.Serialize(f)
+	suite.NoError(err)
+
+	fJson, err := json.Marshal(fi)
+	suite.NoError(err)
+
+	// zork follows admin account and local_account_1
+	suite.Equal(`{"@context":"https://www.w3.org/ns/activitystreams","items":["http://localhost:8080/users/admin","http://localhost:8080/users/1happyturtle"],"type":"Collection"}`, string(fJson))
+}
+
+func TestFollowingTestSuite(t *testing.T) {
+	suite.Run(t, &FollowingTestSuite{})
+}
