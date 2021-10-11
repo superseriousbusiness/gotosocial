5fe12b2fb009a89a3e5c08380adddad661dbec0b
tsmethurst <tobi.smethurst@protonmail.com>
2021-10-11 17:01:25 +0200

    add user module / api path

---
+++ internal/api/client/user/user.go
@@ -0,0 +1,55 @@
+/*
+   GoToSocial
+   Copyright (C) 2021 GoToSocial Authors admin@gotosocial.org
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package user
+
+import (
+	"net/http"
+
+	"github.com/superseriousbusiness/gotosocial/internal/api"
+	"github.com/superseriousbusiness/gotosocial/internal/config"
+	"github.com/superseriousbusiness/gotosocial/internal/processing"
+	"github.com/superseriousbusiness/gotosocial/internal/router"
+)
+
+const (
+	// BasePath is the base URI path for this module
+	BasePath = "/api/v1/user"
+	// PasswordChangePath is the path for POSTing a password change request.
+	PasswordChangePath = BasePath + "/password_change"
+)
+
+// Module implements the ClientAPIModule interface
+type Module struct {
+	config    *config.Config
+	processor processing.Processor
+}
+
+// New returns a new user module
+func New(config *config.Config, processor processing.Processor) api.ClientModule {
+	return &Module{
+		config:    config,
+		processor: processor,
+	}
+}
+
+// Route attaches all routes from this module to the given router
+func (m *Module) Route(r router.Router) error {
+	r.AttachHandler(http.MethodPost, PasswordChangePath, m.PasswordChangePOSTHandler)
+	return nil
+}
