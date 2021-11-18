e3f38ceaf70b228bdab9c3d4c3e6fa07addc5c8e
forest <forest.n.johnson@gmail.com>
2021-11-17 21:27:13 -0600

    refining and cleaning up sqlite permissions error prediction logic

--- internal/db/bundb/bundb.go
+++ internal/db/bundb/bundb.go
@@ -140,20 +140,31 @@ func NewBunDBService(ctx context.Context, c *config.Config) (db.DB, error) {
 			return nil, fmt.Errorf("could not open sqlite db: could not determine parent folder permissions for '%s': %s", providedPath, err)
 		}
 		parentFolderStat := parentFolderInfo.Sys().(*syscall.Stat_t)
-		readWritableBecauseUser := ((parentFolderInfo.Mode() & 0600) == 0600) && os.Geteuid() == int(parentFolderStat.Uid)
-		readWritableBecauseGroup := ((parentFolderInfo.Mode() & 0060) == 0060) && os.Getegid() == int(parentFolderStat.Gid)
-		readWritableBecauseEveryone := ((parentFolderInfo.Mode() & 0006) == 0006)
+		processUserOwnsFolder := os.Geteuid() == int(parentFolderStat.Uid)
+		processIsRunningAsRoot := os.Geteuid() == 0
+		processHasGroupMatchForFolder := os.Getegid() == int(parentFolderStat.Gid)
+		folderAllowsOwnerToReadAndWrite := (parentFolderInfo.Mode() & 0600) == 0600
+		folderAllowsGroupMembersToReadAndWrite := (parentFolderInfo.Mode() & 0060) == 0060
+		folderAllowsAnyoneToReadAndWrite := (parentFolderInfo.Mode() & 0006) == 0006
+		readWritableBecauseUser := processUserOwnsFolder && folderAllowsOwnerToReadAndWrite
+		// could use os.Getgroups() here to determine if the process user has membership in the group for the folder...
+		// but does it really matter that much just for a warning message? 99% of the time the process UID should own the folder anyways
+		readWritableBecauseGroup := processHasGroupMatchForFolder && folderAllowsGroupMembersToReadAndWrite
+
+		logrus.Infof("attempting to open sqlite database at %s", providedPath)
+		if !(processIsRunningAsRoot || readWritableBecauseUser || readWritableBecauseGroup || folderAllowsAnyoneToReadAndWrite) {
+			logrus.Warnf("WARNING: parent folder permissions for '%s' do not seem to allow us to read and write", providedPath)
+			if !processUserOwnsFolder {
+				logrus.Warnf("GoToSocial is running as user id %d, however the folder is owned by user %d", os.Geteuid(), int(parentFolderStat.Uid))
+			} else if !folderAllowsOwnerToReadAndWrite {
+				logrus.Warnf("GoToSocial (user id %d) owns the folder, however the owner is not allowed to read and write", os.Geteuid())
+			}
+		}
 
 		// Append our own SQLite preferences
 		c.DBConfig.Address = "file:" + c.DBConfig.Address + "?cache=shared"
 
-		logrus.Infof("attempting to open sqlite database with address %s", c.DBConfig.Address)
-		if !(readWritableBecauseUser || readWritableBecauseGroup || readWritableBecauseEveryone) {
-			logrus.Warnf("WARNING: parent folder permissions for '%s' do not seem to allow us to read and write", providedPath)
-		}
-
 		// Open new DB instance
-
 		sqldb, err = sql.Open("sqlite", c.DBConfig.Address)
 		if err != nil {
 			return nil, fmt.Errorf("could not open sqlite db: %s", err)
