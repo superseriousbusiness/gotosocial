85d4ad537f01239cb19727284d05c35562e89881
tsmethurst <tobi.smethurst@protonmail.com>
2022-03-06 14:15:33 +0100

    index commonly used fields

--- internal/db/bundb/migrations/20220305130328_database_optimizations.go
+++ internal/db/bundb/migrations/20220305130328_database_optimizations.go
@@ -28,13 +28,203 @@ import (
 func init() {
 	up := func(ctx context.Context, db *bun.DB) error {
 		return db.RunInTx(ctx, nil, func(ctx context.Context, tx bun.Tx) error {
-			// we commonly select notifications by their id, and their target_account_id,
-			// so it makes sense to create an index for this
+			// ACCOUNTS are often selected by URI, username, or domain
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.Account{}).
+				Index("accounts_uri_idx").
+				Column("uri").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.Account{}).
+				Index("accounts_username_idx").
+				Column("username").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.Account{}).
+				Index("accounts_domain_idx").
+				Column("domain").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			// NOTIFICATIONS are commonly selected by their target_account_id
 			if _, err := tx.
 				NewCreateIndex().
 				Model(&gtsmodel.Notification{}).
-				Index("notifications_id_target_account_id_idx").
-				Column("id", "target_account_id").
+				Index("notifications_target_account_id_idx").
+				Column("target_account_id").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			// STATUSES are selected in a few different ways, so we need a few indexes
+			// to avoid queries becoming painfully slow as more statuses get stored
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.Status{}).
+				Index("statuses_uri_idx").
+				Column("uri").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.Status{}).
+				Index("statuses_account_id_idx").
+				Column("account_id").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.Status{}).
+				Index("statuses_in_reply_to_id_idx").
+				Column("in_reply_to_id").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.Status{}).
+				Index("statuses_boost_of_id_idx").
+				Column("boost_of_id").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.Status{}).
+				Index("statuses_visibility_idx").
+				Column("visibility").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			// DOMAIN BLOCKS are almost always selected by their domain
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.DomainBlock{}).
+				Index("domain_blocks_domain_idx").
+				Column("domain").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			// INSTANCES are usually selected by their domain
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.Instance{}).
+				Index("instances_domain_idx").
+				Column("domain").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			// STATUS FAVES are almost always selected by their target status
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.StatusFave{}).
+				Index("status_faves_status_id_idx").
+				Column("status_id").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			// MENTIONS are almost always selected by their originating status
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.Mention{}).
+				Index("mentions_status_id_idx").
+				Column("status_id").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			// FOLLOW_REQUESTS and FOLLOWS are usually selected by who they originate from, and who they target
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.FollowRequest{}).
+				Index("follow_requests_account_id_idx").
+				Column("account_id").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.FollowRequest{}).
+				Index("follow_requests_target_account_id_idx").
+				Column("target_account_id").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.Follow{}).
+				Index("follows_account_id_idx").
+				Column("account_id").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.Follow{}).
+				Index("follows_target_account_id_idx").
+				Column("target_account_id").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			// BLOCKS are usually selected simultaneously by who they originate from and who they target
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.Block{}).
+				Index("blocks_account_id_target_account_id_idx").
+				Column("account_id", "target_account_id").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			// MEDIA ATTACHMENTS are often selected by status ID, the account that owns the attachment
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.MediaAttachment{}).
+				Index("media_attachments_status_id_idx").
+				Column("status_id").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.MediaAttachment{}).
+				Index("media_attachments_account_id_idx").
+				Column("account_id").
+				Exec(ctx); err != nil {
+				return err
+			}
+
+			// for media cleanup jobs, attachments will be selected on a bunch of fields so make an index of this...
+			if _, err := tx.
+				NewCreateIndex().
+				Model(&gtsmodel.MediaAttachment{}).
+				Index("media_attachments_cleanup_idx").
+				Column("cached", "avatar", "header", "created_at", "remote_url").
 				Exec(ctx); err != nil {
 				return err
 			}
