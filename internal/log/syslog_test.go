1cfbb33b3ccc0663a2583b03b5965d15d85e88d9
tsmethurst <tobi.smethurst@protonmail.com>
2022-04-26 13:57:00 +0200

    test syslog w/ unix socket + long (trimmed) msg

--- internal/log/syslog_test.go
+++ internal/log/syslog_test.go
@@ -19,8 +19,12 @@
 package log_test
 
 import (
+	"os"
+	"path"
+	"regexp"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/suite"
@@ -29,6 +33,8 @@ import (
 	"gopkg.in/mcuadros/go-syslog.v2"
 	"gopkg.in/mcuadros/go-syslog.v2/format"
 )
+
+var longMessage = string(make([]byte, 8192))
 
 type SyslogTestSuite struct {
 	suite.Suite
@@ -61,17 +67,45 @@ func (suite *SyslogTestSuite) TearDownTest() {
 func (suite *SyslogTestSuite) TestSyslog() {
 	logrus.Warn("this is a test of the emergency broadcast system!")
 
-	message := <-suite.syslogChannel
-	suite.Contains(message["content"], "msg=this is a test of the emergency broadcast system!")
+	entry := <-suite.syslogChannel
+	suite.Regexp(regexp.MustCompile(`time=.* msg=this is a test of the emergency broadcast system! func=.*`), entry["content"])
 }
 
 func (suite *SyslogTestSuite) TestSyslogLongMessage() {
-	longMessage := "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit ut aliquam purus sit. Fringilla est ullamcorper eget nulla facilisi. Nisi vitae suscipit tellus mauris a diam maecenas sed. Sed adipiscing diam donec adipiscing tristique risus nec feugiat. Consequat id porta nibh venenatis cras sed felis eget velit. Dignissim sodales ut eu sem integer vitae justo eget magna. Pellentesque habitant morbi tristique senectus et netus et malesuada. Semper eget duis at tellus at urna condimentum mattis. Ac auctor augue mauris augue. Erat velit scelerisque in dictum non consectetur a erat nam. Accumsan in nisl nisi scelerisque eu ultrices. Amet justo donec enim diam vulputate ut pharetra sit amet. Elit pellentesque habitant morbi tristique senectus et. Elementum nibh tellus molestie nunc. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. Dictum fusce ut placerat orci nulla pellentesque dignissim. Vel elit scelerisque mauris pellentesque. Dignissim cras tincidunt lobortis feugiat vivamus. Massa enim nec dui nunc mattis. Sed sed risus pretium quam vulputate dignissim suspendisse. Purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Vulputate enim nulla aliquet porttitor lacus. Sed lectus vestibulum mattis ullamcorper. Lorem ipsum dolor sit amet. Egestas diam in arcu cursus euismod quis viverra. Varius sit amet mattis vulputate enim nulla. Proin sagittis nisl rhoncus mattis rhoncus urna neque. Arcu non odio euismod lacinia at quis risus. Nulla facilisi cras fermentum odio eu feugiat pretium. Molestie a iaculis at erat pellentesque adipiscing commodo elit. Etiam tempor orci eu lobortis. Eget nulla facilisi etiam dignissim diam. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et. Commodo quis imperdiet massa tincidunt nunc pulvinar. Ullamcorper morbi tincidunt ornare massa eget egestas purus. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Vel facilisis volutpat est velit egestas. Diam sit amet nisl suscipit adipiscing bibendum est. Phasellus egestas tellus rutrum tellus. Eu ultrices vitae auctor eu augue ut lectus. Lorem sed risus ultricies tristique nulla aliquet enim. Eget nunc scelerisque viverra mauris in aliquam sem. Lorem ipsum dolor sit amet. Sit amet porttitor eget dolor morbi. Lacus suspendisse faucibus interdum posuere lorem ipsum. Nec ullamcorper sit amet risus nullam eget felis eget. Dignissim convallis aenean et tortor. Purus in mollis nunc sed id semper risus in. Amet aliquam id diam maecenas ultricies mi. Orci phasellus egestas tellus rutrum tellus. Tristique sollicitudin nibh sit amet commodo. Aliquet bibendum enim facilisis gravida. Morbi tempus iaculis urna id volutpat. Integer eget aliquet nibh praesent tristique magna sit amet purus. Eu augue ut lectus arcu. Rhoncus dolor purus non enim praesent elementum facilisis. A condimentum vitae sapien pellentesque habitant morbi tristique. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Facilisis mauris sit amet massa vitae tortor condimentum lacinia quis. Morbi non arcu risus quis varius quam quisque. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Neque volutpat ac tincidunt vitae semper quis lectus nulla at. Vestibulum sed arcu non odio euismod lacinia at quis. Aenean sed adipiscing diam donec. Consequat ac felis donec et odio pellentesque diam. Placerat orci nulla pellentesque dignissim enim sit amet. Tempor commodo ullamcorper a lacus vestibulum sed arcu. Mollis aliquam ut porttitor leo a diam sollicitudin tempor. Aliquet risus feugiat in ante metus dictum at tempor commodo. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Mattis nunc sed blandit libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit ut aliquam purus sit. Fringilla est ullamcorper eget nulla facilisi. Nisi vitae suscipit tellus mauris a diam maecenas sed. Sed adipiscing diam donec adipiscing tristique risus nec feugiat. Consequat id porta nibh venenatis cras sed felis eget velit. Dignissim sodales ut eu sem integer vitae justo eget magna. Pellentesque habitant morbi tristique senectus et netus et malesuada. Semper eget duis at tellus at urna condimentum mattis. Ac auctor augue mauris augue. Erat velit scelerisque in dictum non consectetur a erat nam. Accumsan in nisl nisi scelerisque eu ultrices. Amet justo donec enim diam vulputate ut pharetra sit amet. Elit pellentesque habitant morbi tristique senectus et. Elementum nibh tellus molestie nunc. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. Dictum fusce ut placerat orci nulla pellentesque dignissim. Vel elit scelerisque mauris pellentesque. Dignissim cras tincidunt lobortis feugiat vivamus. Massa enim nec dui nunc mattis. Sed sed risus pretium quam vulputate dignissim suspendisse. Purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Vulputate enim nulla aliquet porttitor lacus. Sed lectus vestibulum mattis ullamcorper. Lorem ipsum dolor sit amet. Egestas diam in arcu cursus euismod quis viverra. Varius sit amet mattis vulputate enim nulla. Proin sagittis nisl rhoncus mattis rhoncus urna neque. Arcu non odio euismod lacinia at quis risus. Nulla facilisi cras fermentum odio eu feugiat pretium. Molestie a iaculis at erat pellentesque adipiscing commodo elit. Etiam tempor orci eu lobortis. Eget nulla facilisi etiam dignissim diam. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et. Commodo quis imperdiet massa tincidunt nunc pulvinar. Ullamcorper morbi tincidunt ornare massa eget egestas purus. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Vel facilisis volutpat est velit egestas. Diam sit amet nisl suscipit adipiscing bibendum est. Phasellus egestas tellus rutrum tellus. Eu ultrices vitae auctor eu augue ut lectus. Lorem sed risus ultricies tristique nulla aliquet enim. Eget nunc scelerisque viverra mauris in aliquam sem. Lorem ipsum dolor sit amet. Sit amet porttitor eget dolor morbi. Lacus suspendisse faucibus interdum posuere lorem ipsum. Nec ullamcorper sit amet risus nullam eget felis eget. Dignissim convallis aenean et tortor. Purus in mollis nunc sed id semper risus in. Amet aliquam id diam maecenas ultricies mi. Orci phasellus egestas tellus rutrum tellus. Tristique sollicitudin nibh sit amet commodo. Aliquet bibendum enim facilisis gravida. Morbi tempus iaculis urna id volutpat. Integer eget aliquet nibh praesent tristique magna sit amet purus. Eu augue ut lectus arcu. Rhoncus dolor purus non enim praesent elementum facilisis. A condimentum vitae sapien pellentesque habitant morbi tristique. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Facilisis mauris sit amet massa vitae tortor condimentum lacinia quis. Morbi non arcu risus quis varius quam quisque. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Neque volutpat ac tincidunt vitae semper quis lectus nulla at. Vestibulum sed arcu non odio euismod lacinia at quis. Aenean sed adipiscing diam donec. Consequat ac felis donec et odio pellentesque diam. Placerat orci nulla pellentesque dignissim enim sit amet. Tempor commodo ullamcorper a lacus vestibulum sed arcu. Mollis aliquam ut porttitor leo a diam sollicitudin tempor. Aliquet risus feugiat in ante metus dictum at tempor commodo. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Mattis nunc sed blandit libero."
-	
 	logrus.Warn(longMessage)
 
-	message := <-suite.syslogChannel
-	suite.Contains(message["content"], "this is a test of the emergency broadcast system!")
+	entry := <-suite.syslogChannel
+	suite.Regexp(regexp.MustCompile(`time=.* msg=\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\.\.\. func=.*`), entry["content"])
+}
+
+func (suite *SyslogTestSuite) TestSyslogLongMessageUnixgram() {
+	socketPath := path.Join(os.TempDir(), uuid.NewString())
+	defer func() {
+		if err := os.Remove(socketPath); err != nil {
+			panic(err)
+		}
+	}()
+
+	server, channel, err := testrig.InitTestSyslogUnixgram(socketPath)
+	if err != nil {
+		panic(err)
+	}
+	syslogServer := server
+	syslogChannel := channel
+
+	viper.Set(config.Keys.SyslogEnabled, true)
+	viper.Set(config.Keys.SyslogProtocol, "unixgram")
+	viper.Set(config.Keys.SyslogAddress, socketPath)
+	testrig.InitTestLog()
+
+	logrus.Warn(longMessage)
+
+	entry := <-syslogChannel
+	suite.Regexp(regexp.MustCompile(`time=.* msg=\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\.\.\. func=.*`), entry["content"])
+
+	if err := syslogServer.Kill(); err != nil {
+		panic(err)
+	}
 }
 
 func TestSyslogTestSuite(t *testing.T) {
