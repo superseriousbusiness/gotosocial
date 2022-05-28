78725747e4a6014586a808d85f82a8f8a6323120
tsmethurst <tobi.smethurst@protonmail.com>
2022-05-28 16:05:38 +0200

    rearrange attachment fields a bit

--- internal/api/model/attachment.go
+++ internal/api/model/attachment.go
@@ -69,20 +69,20 @@ type Attachment struct {
 	// The location of the original full-size attachment.
 	// example: https://example.org/fileserver/some_id/attachments/some_id/original/attachment.jpeg
 	URL string `json:"url"`
+	// A shorter URL for the attachment.
+	// In our case, we just give the URL again since we don't create smaller URLs.
+	TextURL string `json:"text_url"`
 	// The location of a scaled-down preview of the attachment.
 	// example: https://example.org/fileserver/some_id/attachments/some_id/small/attachment.jpeg
 	PreviewURL string `json:"preview_url"`
 	// The location of the full-size original attachment on the remote server.
 	// Only defined for instances other than our own.
 	// example: https://some-other-server.org/attachments/original/ahhhhh.jpeg
-	RemoteURL string `json:"remote_url,omitempty"`
+	RemoteURL string `json:"remote_url"`
 	// The location of a scaled-down preview of the attachment on the remote server.
 	// Only defined for instances other than our own.
 	// example: https://some-other-server.org/attachments/small/ahhhhh.jpeg
-	PreviewRemoteURL string `json:"preview_remote_url,omitempty"`
-	// A shorter URL for the attachment.
-	// Not currently used.
-	TextURL string `json:"text_url"`
+	PreviewRemoteURL string `json:"preview_remote_url"`
 	// Metadata for this attachment.
 	Meta MediaMeta `json:"meta,omitempty"`
 	// Alt text that describes what is in the media attachment.
