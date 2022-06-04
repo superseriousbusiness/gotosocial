b1e0da6eddd592aaf8b8d7baaad69a972e7baa53
tsmethurst <tobi.smethurst@protonmail.com>
2022-06-04 19:56:19 +0200

    update time marshalling format

--- internal/util/time_test.go
+++ internal/util/time_test.go
@@ -33,19 +33,19 @@ type TimeSuite struct {
 func (suite *TimeSuite) TestISO8601Format1() {
 	testTime := testrig.TimeMustParse("2022-05-17T13:10:59Z")
 	testTimeString := util.FormatISO8601(testTime)
-	suite.Equal("2022-05-17T13:10:59.00Z", testTimeString)
+	suite.Equal("2022-05-17T13:10:59.000Z", testTimeString)
 }
 
 func (suite *TimeSuite) TestISO8601Format2() {
 	testTime := testrig.TimeMustParse("2022-05-09T07:34:35+02:00")
 	testTimeString := util.FormatISO8601(testTime)
-	suite.Equal("2022-05-09T05:34:35.00Z", testTimeString)
+	suite.Equal("2022-05-09T05:34:35.000Z", testTimeString)
 }
 
 func (suite *TimeSuite) TestISO8601Format3() {
 	testTime := testrig.TimeMustParse("2021-10-04T10:52:36+02:00")
 	testTimeString := util.FormatISO8601(testTime)
-	suite.Equal("2021-10-04T08:52:36.00Z", testTimeString)
+	suite.Equal("2021-10-04T08:52:36.000Z", testTimeString)
 }
 
 func TestTimeSuite(t *testing.T) {
