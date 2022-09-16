4c6f6f3d40893c8813b30cc17d30a053fe6a2a4f
tsmethurst <tobi.smethurst@protonmail.com>
2022-09-16 10:51:39 +0200

    linter

--- internal/api/errorhandling.go
+++ internal/api/errorhandling.go
@@ -88,7 +88,7 @@ func genericErrorHandler(c *gin.Context, instanceGet func(ctx context.Context, d
 func ErrorHandler(c *gin.Context, errWithCode gtserror.WithCode, instanceGet func(ctx context.Context, domain string) (*apimodel.Instance, gtserror.WithCode)) {
 	// set the error on the gin context so that it can be logged
 	// in the gin logger middleware (internal/router/logger.go)
-	c.Error(errWithCode)
+	c.Error(errWithCode) //nolint:errcheck
 
 	// discover if we're allowed to serve a nice html error page,
 	// or if we should just use a json. Normally we would want to
