c1585d5f8a57256a330aae4a322468aaccf9d3fa
tobi <31960611+tsmethurst@users.noreply.github.com>
2022-09-17 19:12:12 +0200

    [bugfix] Fix domains not being unblockable, log internal server errors from API (#833)
    * log internal server errors from 500 api calls
    
    * don't exec into nil dest
    
    * don't exec into nil dest
    
    * log error in router logger not api errorhandling
    
    * update logging a tad
    
    * linter

--- internal/router/logger.go
+++ internal/router/logger.go
@@ -72,28 +72,20 @@ func loggingMiddleware(c *gin.Context) {
 		fields[4] = kv.Field{"statusCode", code}
 		fields[5] = kv.Field{"path", path}
 
-		var lvl level.LEVEL
+		// Create log entry with fields
+		l := log.WithFields(fields...)
 
 		// Default is info
-		lvl = level.INFO
+		lvl := level.INFO
 
 		if code >= 500 {
 			// This is a server error
 			lvl = level.ERROR
-
-			if len(c.Errors) > 0 {
-				// Add an error string log field
-				fields = append(fields, kv.Field{
-					"error", c.Errors.String(),
-				})
-			}
+			l = l.WithField("error", c.Errors)
 		}
 
 		// Generate a nicer looking bytecount
 		size := bytesize.Size(c.Writer.Size())
-
-		// Create log entry with fields
-		l := log.WithFields(fields...)
 
 		// Finally, write log entry with status text body size
 		l.Logf(lvl, "%s: wrote %s", http.StatusText(code), size)
