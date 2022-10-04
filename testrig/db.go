b77de7e397b1afeac23340b9f1eea1a1e90d86b9
tsmethurst <tobi.smethurst@protonmail.com>
2022-10-04 11:04:09 +0200

    remove workaround

--- testrig/db.go
+++ testrig/db.go
@@ -187,7 +187,7 @@ func StandardDBSetup(db db.DB, accounts map[string]*gtsmodel.Account) {
 	}
 
 	for _, v := range NewTestStatuses() {
-		if err := db.PutStatus(ctx, v); err != nil {
+		if err := db.Put(ctx, v); err != nil {
 			log.Panic(err)
 		}
 	}
