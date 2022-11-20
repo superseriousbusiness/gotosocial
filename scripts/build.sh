979f00544f97d6dae8b7bc9b5598cc46f20fdb2f
kim <grufwub@gmail.com>
2022-11-20 15:46:13 +0000

    undo my accidental inclusion of custom version string...
    Signed-off-by: kim <grufwub@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,6 @@ set -eu
 # DEBUG returns whether DEBUG build is enabled.
 DEBUG() { [ ! -z "${DEBUG-}" ]; }
 
-export VERSION='v0.6.0-dev-kims-secret-sauce'
 CGO_ENABLED=0 go build -trimpath \
                        -tags "netgo osusergo static_build kvformat $(DEBUG && echo 'debugenv')" \
                        -ldflags="-s -w -extldflags '-static' -X 'main.Version=${VERSION:-$(git describe --tags --abbrev=0)}'" \
