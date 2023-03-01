1126136e8ec44f9499c5b0ec430313c94b266e9c
tsmethurst <tobi.smethurst@protonmail.com>
2023-03-01 18:32:06 +0100

    update migration to work with both pg and sqlite

--- internal/db/bundb/migrations/20230126170719_format_to_content_type.go
+++ internal/db/bundb/migrations/20230126170719_format_to_content_type.go
@@ -23,24 +23,50 @@ import (
 	"strings"
 
 	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect"
 )
 
 func init() {
 	up := func(ctx context.Context, db *bun.DB) error {
 		return db.RunInTx(ctx, nil, func(ctx context.Context, tx bun.Tx) error {
-			var err error
-			_, err = db.ExecContext(ctx, "ALTER TABLE ? ADD COLUMN ? TEXT", bun.Ident("accounts"), bun.Ident("status_content_type"))
-			if err != nil && !(strings.Contains(err.Error(), "already exists") || strings.Contains(err.Error(), "duplicate column name") || strings.Contains(err.Error(), "SQLSTATE 42701")) {
+			// Create a new column for status_content_type.
+			if _, err := tx.ExecContext(ctx, "ALTER TABLE ? ADD COLUMN ? TEXT", bun.Ident("accounts"), bun.Ident("status_content_type")); err != nil &&
+				!(strings.Contains(err.Error(), "already exists") || strings.Contains(err.Error(), "duplicate column name") || strings.Contains(err.Error(), "SQLSTATE 42701")) {
 				return err
 			}
-			_, err = db.ExecContext(ctx, "UPDATE ? SET ? = 'text/' || ? WHERE ? IS NOT NULL", bun.Ident("accounts"), bun.Ident("status_content_type"), bun.Ident("status_format"), bun.Ident("status_format"))
-			if err != nil {
+
+			// Port values into this column from the old status_format column,
+			// prepending 'text/' to the value to derive the mime type.
+			//
+			// Eg, 'plain' status_format becomes 'text/plain' status_content_type.
+			q := tx.
+				NewUpdate().
+				Table("accounts").
+				Where("? IS NOT NULL", bun.Ident("status_format"))
+
+			// We need to switch here because Postgres and SQLite
+			// have different syntaxes for concatenation.
+			switch tx.Dialect().Name() {
+			case dialect.SQLite:
+				q = q.
+					Set("? = ? || ?", bun.Ident("status_content_type"), "text/", bun.Ident("status_format"))
+			case dialect.PG:
+				q = q.
+					Set("? = CONCAT(?, ?)", bun.Ident("status_content_type"), "text/", bun.Ident("status_format"))
+			default:
+				panic("db conn was neither pg not sqlite")
+			}
+
+			if _, err := q.Exec(ctx); err != nil {
 				return err
 			}
-			_, err = db.ExecContext(ctx, "ALTER TABLE ? DROP COLUMN ?", bun.Ident("accounts"), bun.Ident("status_format"))
-			if err != nil {
+
+			// Drop the old status_format column.
+			if _, err := tx.ExecContext(ctx, "ALTER TABLE ? DROP COLUMN ?", bun.Ident("accounts"), bun.Ident("status_format")); err != nil &&
+				!(strings.Contains(err.Error(), "no such column") || strings.Contains(err.Error(), "does not exist") || strings.Contains(err.Error(), "SQLSTATE 42703")) {
 				return err
 			}
+
 			return nil
 		})
 	}
