b9f390bde42f7f2e0f5b73f7661135f46b8610de
kim <grufwub@gmail.com>
2023-04-10 22:19:08 +0100

    add status fetched_at migration
    Signed-off-by: kim <grufwub@gmail.com>

---
+++ internal/db/bundb/migrations/20230401142420_add_status_fetched_at.go
@@ -0,0 +1,26 @@
+package migrations
+
+import (
+	"context"
+
+	"github.com/uptrace/bun"
+)
+
+func init() {
+	up := func(ctx context.Context, db *bun.DB) error {
+		return db.RunInTx(ctx, nil, func(ctx context.Context, tx bun.Tx) error {
+			_, err := tx.ExecContext(ctx, "ALTER TABLE ? ADD COLUMN ? TIMESTAMP", bun.Ident("statuses"), bun.Ident("fetched_at"))
+			return err
+		})
+	}
+
+	down := func(ctx context.Context, db *bun.DB) error {
+		return db.RunInTx(ctx, nil, func(ctx context.Context, tx bun.Tx) error {
+			return nil
+		})
+	}
+
+	if err := Migrations.Register(up, down); err != nil {
+		panic(err)
+	}
+}
