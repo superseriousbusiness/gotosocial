66120b80751cea7d004b7c3e09b06c01752c37bb
kim <grufwub@gmail.com>
2023-05-14 13:45:29 +0100

    remove accidentally comitted build-script changes
    Signed-off-by: kim <grufwub@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,7 @@ set -eu
 # DEBUG returns whether DEBUG build is enabled.
 DEBUG() { [ ! -z "${DEBUG-}" ]; }
 
-CGO_ENABLED=0 go build -trimpath -v \
-                       -tags "netgo osusergo static_build kvformat notracing $(DEBUG && echo 'debugenv')" \
+CGO_ENABLED=0 go build -trimpath \
+                       -tags "netgo osusergo static_build kvformat $(DEBUG && echo 'debugenv')" \
                        -ldflags="-s -w -extldflags '-static' -X 'main.Version=${VERSION:-$(git describe --tags --abbrev=0)}'" \
-                       -gcflags=all='-B -C -l=4' \
                        ./cmd/gotosocial
