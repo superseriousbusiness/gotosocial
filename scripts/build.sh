9bd03e122e9c0e9c4dc3c4683b834c4a3bd673ea
tobi <31960611+tsmethurst@users.noreply.github.com>
2023-08-01 14:47:53 +0200

    [feature] Set timezone in Docker using TZ env variable (#2050)

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ set -e
 log_exec() { echo "$ ${*}"; "$@"; }
 
 # Grab environment variables and set defaults + requirements.
-GO_BUILDTAGS="${GO_BUILDTAGS-} netgo osusergo static_build kvformat"
+GO_BUILDTAGS="${GO_BUILDTAGS-} netgo osusergo static_build kvformat timetzdata"
 GO_LDFLAGS="${GO_LDFLAGS-} -s -w -extldflags '-static' -X 'main.Version=${VERSION:-$(git describe --tags --abbrev=0)}'"
 GO_GCFLAGS=${GO_GCFLAGS-}
 
@@ -16,6 +16,7 @@ GO_GCFLAGS=${GO_GCFLAGS-}
 
 # Available Go build tags, with explanation, followed by benefits of enabling it:
 # - kvformat:    enables prettier output of log fields                       (slightly better performance)
+# - timetzdata:  embed timezone database inside binary                       (allow setting local time inside Docker containers, at cost of 450KB)
 # - notracing:   disables compiling-in otel tracing support                  (reduced binary size, better performance)
 # - noerrcaller: disables caller function prefix in errors                   (slightly better performance, at cost of err readability)
 # - debug:       enables /debug/pprof endpoint                               (adds debug, at performance cost)
