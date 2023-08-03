3ab731f006db445aebac0301de561a7236f55921
kim <grufwub@gmail.com>
2023-08-03 15:06:47 +0100

    add comments regarding AllowZero usage
    Signed-off-by: kim <grufwub@gmail.com>

--- internal/cache/gts.go
+++ internal/cache/gts.go
@@ -268,7 +268,7 @@ func (c *GTSCaches) initAccount() {
 		{Name: "ID"},
 		{Name: "URI"},
 		{Name: "URL"},
-		{Name: "Username.Domain", AllowZero: true},
+		{Name: "Username.Domain", AllowZero: true /* domain can be zero i.e. "" */},
 		{Name: "PublicKeyURI"},
 		{Name: "InboxURI"},
 		{Name: "OutboxURI"},
@@ -371,7 +371,7 @@ func (c *GTSCaches) initEmoji() {
 	c.emoji = result.New([]result.Lookup{
 		{Name: "ID"},
 		{Name: "URI"},
-		{Name: "Shortcode.Domain", AllowZero: true},
+		{Name: "Shortcode.Domain", AllowZero: true /* domain can be zero i.e. "" */},
 		{Name: "ImageStaticURL"},
 		{Name: "CategoryID", Multi: true},
 	}, func(e1 *gtsmodel.Emoji) *gtsmodel.Emoji {
