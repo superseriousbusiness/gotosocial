9b2bb4b93cf168c9eb177a7df281e0c831ff3373
kim <grufwub@gmail.com>
2023-08-08 11:49:05 +0100

    update test scripts to accept custom arguments
    Signed-off-by: kim <grufwub@gmail.com>

--- test/run-sqlite.sh
+++ test/run-sqlite.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# Ensure test args are set.
+ARGS=${@}; [ -z "$ARGS" ] && \
+ARGS='./...'
+
+# Run the SQLite tests.
 GTS_DB_TYPE=sqlite \
 GTS_DB_ADDRESS=':memory:' \
-go test ./... ${@}
+go test ${ARGS}
