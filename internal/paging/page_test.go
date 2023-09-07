b46c6e56549426d0690e02418cd0547bec3ead8b
kim <grufwub@gmail.com>
2023-09-07 12:01:35 +0100

    much more robust paging tests
    Signed-off-by: kim <grufwub@gmail.com>

--- internal/paging/page_test.go
+++ internal/paging/page_test.go
@@ -18,154 +18,281 @@
 package paging_test
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
+	"github.com/oklog/ulid"
 	"github.com/superseriousbusiness/gotosocial/internal/paging"
 	"golang.org/x/exp/slices"
 )
 
+// random reader according to current-time source seed.
+var randRd = rand.New(rand.NewSource(time.Now().Unix()))
+
 type Case struct {
 	// Name is the test case name.
 	Name string
+
+	// Page to use for test.
+	Page *paging.Page
 
 	// Input contains test case input ID slice.
 	Input []string
 
 	// Expect contains expected test case output.
 	Expect []string
-
-	// Page contains the paging function to use.
-	Page func([]string) []string
 }
 
-var cases = []Case{
-	{
-		Name: "min_id and max_id set",
-		Input: []string{
-			"064Q5D7VG6TPPQ46T09MHJ96FW",
-			"064Q5D7VGPTC4NK5T070VYSSF8",
-			"064Q5D7VH5F0JXG6W5NCQ3JCWW",
-			"064Q5D7VHMSW9DF3GCS088VAZC",
-			"064Q5D7VJ073XG9ZTWHA2KHN10",
-			"064Q5D7VJADJTPA3GW8WAX10TW",
-			"064Q5D7VJMWXZD3S1KT7RD51N8",
-			"064Q5D7VJYFBYSAH86KDBKZ6AC",
-			"064Q5D7VK8H7WMJS399SHEPCB0",
-			"064Q5D7VKG5EQ43TYP71B4K6K0",
-		},
-		Expect: []string{
-			"064Q5D7VGPTC4NK5T070VYSSF8",
-			"064Q5D7VH5F0JXG6W5NCQ3JCWW",
-			"064Q5D7VHMSW9DF3GCS088VAZC",
-			"064Q5D7VJ073XG9ZTWHA2KHN10",
-			"064Q5D7VJADJTPA3GW8WAX10TW",
-			"064Q5D7VJMWXZD3S1KT7RD51N8",
-			"064Q5D7VJYFBYSAH86KDBKZ6AC",
-			"064Q5D7VK8H7WMJS399SHEPCB0",
-		},
-		Page: (&paging.Page{
-			Min: paging.MinID("064Q5D7VG6TPPQ46T09MHJ96FW", ""),
-			Max: paging.MaxID("064Q5D7VKG5EQ43TYP71B4K6K0"),
-		}).Page,
-	},
-	{
-		Name: "min_id, max_id and limit set",
-		Input: []string{
-			"064Q5D7VG6TPPQ46T09MHJ96FW",
-			"064Q5D7VGPTC4NK5T070VYSSF8",
-			"064Q5D7VH5F0JXG6W5NCQ3JCWW",
-			"064Q5D7VHMSW9DF3GCS088VAZC",
-			"064Q5D7VJ073XG9ZTWHA2KHN10",
-			"064Q5D7VJADJTPA3GW8WAX10TW",
-			"064Q5D7VJMWXZD3S1KT7RD51N8",
-			"064Q5D7VJYFBYSAH86KDBKZ6AC",
-			"064Q5D7VK8H7WMJS399SHEPCB0",
-			"064Q5D7VKG5EQ43TYP71B4K6K0",
-		},
-		Expect: []string{
-			"064Q5D7VGPTC4NK5T070VYSSF8",
-			"064Q5D7VH5F0JXG6W5NCQ3JCWW",
-			"064Q5D7VHMSW9DF3GCS088VAZC",
-			"064Q5D7VJ073XG9ZTWHA2KHN10",
-			"064Q5D7VJADJTPA3GW8WAX10TW",
-		},
-		Page: (&paging.Page{
-			Min:   paging.MinID("064Q5D7VG6TPPQ46T09MHJ96FW", ""),
-			Max:   paging.MaxID("064Q5D7VKG5EQ43TYP71B4K6K0"),
-			Limit: 5,
-		}).Page,
-	},
-	{
-		Name: "min_id, max_id and too-large limit set",
-		Input: []string{
-			"064Q5D7VG6TPPQ46T09MHJ96FW",
-			"064Q5D7VGPTC4NK5T070VYSSF8",
-			"064Q5D7VH5F0JXG6W5NCQ3JCWW",
-			"064Q5D7VHMSW9DF3GCS088VAZC",
-			"064Q5D7VJ073XG9ZTWHA2KHN10",
-			"064Q5D7VJADJTPA3GW8WAX10TW",
-			"064Q5D7VJMWXZD3S1KT7RD51N8",
-			"064Q5D7VJYFBYSAH86KDBKZ6AC",
-			"064Q5D7VK8H7WMJS399SHEPCB0",
-			"064Q5D7VKG5EQ43TYP71B4K6K0",
-		},
-		Expect: []string{
-			"064Q5D7VGPTC4NK5T070VYSSF8",
-			"064Q5D7VH5F0JXG6W5NCQ3JCWW",
-			"064Q5D7VHMSW9DF3GCS088VAZC",
-			"064Q5D7VJ073XG9ZTWHA2KHN10",
-			"064Q5D7VJADJTPA3GW8WAX10TW",
-			"064Q5D7VJMWXZD3S1KT7RD51N8",
-			"064Q5D7VJYFBYSAH86KDBKZ6AC",
-			"064Q5D7VK8H7WMJS399SHEPCB0",
-		},
-		Page: (&paging.Page{
-			Min:   paging.MinID("064Q5D7VG6TPPQ46T09MHJ96FW", ""),
-			Max:   paging.MaxID("064Q5D7VKG5EQ43TYP71B4K6K0"),
-			Limit: 100,
-		}).Page,
-	},
-	{
-		Name: "since_id and max_id set",
-		Input: []string{
-			"064Q5D7VG6TPPQ46T09MHJ96FW",
-			"064Q5D7VGPTC4NK5T070VYSSF8",
-			"064Q5D7VH5F0JXG6W5NCQ3JCWW",
-			"064Q5D7VHMSW9DF3GCS088VAZC",
-			"064Q5D7VJ073XG9ZTWHA2KHN10",
-			"064Q5D7VJADJTPA3GW8WAX10TW",
-			"064Q5D7VJMWXZD3S1KT7RD51N8",
-			"064Q5D7VJYFBYSAH86KDBKZ6AC",
-			"064Q5D7VK8H7WMJS399SHEPCB0",
-			"064Q5D7VKG5EQ43TYP71B4K6K0",
-		},
-		Expect: []string{
-			"064Q5D7VK8H7WMJS399SHEPCB0",
-			"064Q5D7VJYFBYSAH86KDBKZ6AC",
-			"064Q5D7VJMWXZD3S1KT7RD51N8",
-			"064Q5D7VJADJTPA3GW8WAX10TW",
-			"064Q5D7VJ073XG9ZTWHA2KHN10",
-			"064Q5D7VHMSW9DF3GCS088VAZC",
-			"064Q5D7VH5F0JXG6W5NCQ3JCWW",
-			"064Q5D7VGPTC4NK5T070VYSSF8",
-		},
-		Page: (&paging.Page{
-			Min: paging.MinID("", "064Q5D7VG6TPPQ46T09MHJ96FW"),
-			Max: paging.MaxID("064Q5D7VKG5EQ43TYP71B4K6K0"),
-		}).Page,
-	},
+// CreateCase creates a new test case with random input for function defining test page parameters and expected output.
+func CreateCase(name string, getParams func([]string) (input []string, page *paging.Page, expect []string)) Case {
+	i := randRd.Intn(100)
+	in := generateSlice(i)
+	input, page, expect := getParams(in)
+	return Case{
+		Name:   name,
+		Page:   page,
+		Input:  input,
+		Expect: expect,
+	}
 }
 
 func TestPage(t *testing.T) {
 	for _, c := range cases {
 		t.Run(c.Name, func(t *testing.T) {
 			// Page the input slice.
-			out := c.Page(c.Input)
+			out := c.Page.Page(c.Input)
+
+			// Log the results for case of error returns.
+			t.Logf("\ninput=%v\noutput=%v\nexpected=%v", c.Input, out, c.Expect)
 
 			// Check paged output is as expected.
 			if !slices.Equal(out, c.Expect) {
-				t.Errorf("\nreceived=%v\nexpect%v\n", out, c.Expect)
+				t.Error("unexpected paged output")
 			}
 		})
 	}
+}
+
+var cases = []Case{
+	CreateCase("minID and maxID set", func(ids []string) ([]string, *paging.Page, []string) {
+		// Ensure input slice sorted ascending for min_id
+		slices.SortFunc(ids, func(a, b string) bool {
+			return b < a
+		})
+
+		// Select random indices in slice.
+		minIdx := randRd.Intn(len(ids))
+		maxIdx := randRd.Intn(len(ids))
+
+		// Select the boundaries.
+		minID := ids[minIdx]
+		maxID := ids[maxIdx]
+
+		// Create expected output.
+		expect := slices.Clone(ids)
+		expect = cutLower(expect, minID)
+		expect = cutUpper(expect, maxID)
+		expect = paging.Reverse(expect)
+
+		// Return page and expected IDs.
+		return ids, &paging.Page{
+			Min: paging.MinID(minID, ""),
+			Max: paging.MaxID(maxID),
+		}, expect
+	}),
+	CreateCase("minID, maxID and limit set", func(ids []string) ([]string, *paging.Page, []string) {
+		// Ensure input slice sorted ascending for min_id
+		slices.SortFunc(ids, func(a, b string) bool {
+			return b < a
+		})
+
+		// Select random parameters in slice.
+		minIdx := randRd.Intn(len(ids))
+		maxIdx := randRd.Intn(len(ids))
+		limit := randRd.Intn(len(ids))
+
+		// Select the boundaries.
+		minID := ids[minIdx]
+		maxID := ids[maxIdx]
+
+		// Create expected output.
+		expect := slices.Clone(ids)
+		expect = cutLower(expect, minID)
+		expect = cutUpper(expect, maxID)
+		expect = paging.Reverse(expect)
+
+		// Now limit the slice.
+		if limit < len(expect) {
+			expect = expect[:limit]
+		}
+
+		// Return page and expected IDs.
+		return ids, &paging.Page{
+			Min:   paging.MinID(minID, ""),
+			Max:   paging.MaxID(maxID),
+			Limit: limit,
+		}, expect
+	}),
+	CreateCase("minID, maxID and too-large limit set", func(ids []string) ([]string, *paging.Page, []string) {
+		// Ensure input slice sorted ascending for min_id
+		slices.SortFunc(ids, func(a, b string) bool {
+			return b < a
+		})
+
+		// Select random parameters in slice.
+		minIdx := randRd.Intn(len(ids))
+		maxIdx := randRd.Intn(len(ids))
+
+		// Select the boundaries.
+		minID := ids[minIdx]
+		maxID := ids[maxIdx]
+
+		// Create expected output.
+		expect := slices.Clone(ids)
+		expect = cutLower(expect, minID)
+		expect = cutUpper(expect, maxID)
+		expect = paging.Reverse(expect)
+
+		// Return page and expected IDs.
+		return ids, &paging.Page{
+			Min:   paging.MinID(minID, ""),
+			Max:   paging.MaxID(maxID),
+			Limit: len(ids) * 2,
+		}, expect
+	}),
+	CreateCase("sinceID and maxID set", func(ids []string) ([]string, *paging.Page, []string) {
+		// Ensure input slice sorted descending for since_id
+		slices.SortFunc(ids, func(a, b string) bool {
+			return a < b
+		})
+
+		// Select random indices in slice.
+		sinceIdx := randRd.Intn(len(ids))
+		maxIdx := randRd.Intn(len(ids))
+
+		// Select the boundaries.
+		sinceID := ids[sinceIdx]
+		maxID := ids[maxIdx]
+
+		// Create expected output.
+		expect := slices.Clone(ids)
+		expect = cutLower(expect, maxID)
+		expect = cutUpper(expect, sinceID)
+
+		// Return page and expected IDs.
+		return ids, &paging.Page{
+			Min: paging.MinID("", sinceID),
+			Max: paging.MaxID(maxID),
+		}, expect
+	}),
+	CreateCase("maxID set", func(ids []string) ([]string, *paging.Page, []string) {
+		// Ensure input slice sorted descending for since_id
+		slices.SortFunc(ids, func(a, b string) bool {
+			return a < b
+		})
+
+		// Select random indices in slice.
+		maxIdx := randRd.Intn(len(ids))
+
+		// Select the boundaries.
+		maxID := ids[maxIdx]
+
+		// Create expected output.
+		expect := slices.Clone(ids)
+		expect = cutLower(expect, maxID)
+
+		// Return page and expected IDs.
+		return ids, &paging.Page{
+			Max: paging.MaxID(maxID),
+		}, expect
+	}),
+	CreateCase("sinceID set", func(ids []string) ([]string, *paging.Page, []string) {
+		// Ensure input slice sorted descending for since_id
+		slices.SortFunc(ids, func(a, b string) bool {
+			return a < b
+		})
+
+		// Select random indices in slice.
+		sinceIdx := randRd.Intn(len(ids))
+
+		// Select the boundaries.
+		sinceID := ids[sinceIdx]
+
+		// Create expected output.
+		expect := slices.Clone(ids)
+		expect = cutUpper(expect, sinceID)
+
+		// Return page and expected IDs.
+		return ids, &paging.Page{
+			Min: paging.MinID("", sinceID),
+		}, expect
+	}),
+	CreateCase("minID set", func(ids []string) ([]string, *paging.Page, []string) {
+		// Ensure input slice sorted ascending for min_id
+		slices.SortFunc(ids, func(a, b string) bool {
+			return b < a
+		})
+
+		// Select random indices in slice.
+		minIdx := randRd.Intn(len(ids))
+
+		// Select the boundaries.
+		minID := ids[minIdx]
+
+		// Create expected output.
+		expect := slices.Clone(ids)
+		expect = cutLower(expect, minID)
+		expect = paging.Reverse(expect)
+
+		// Return page and expected IDs.
+		return ids, &paging.Page{
+			Min: paging.MinID(minID, ""),
+		}, expect
+	}),
+}
+
+// cutLower cuts off the lower part of the slice from `bound` downwards.
+func cutLower(in []string, bound string) []string {
+	for i := 0; i < len(in); i++ {
+		if in[i] == bound {
+			return in[i+1:]
+		}
+	}
+	return in
+}
+
+// cutUpper cuts off the upper part of the slice from `bound` onwards.
+func cutUpper(in []string, bound string) []string {
+	for i := 0; i < len(in); i++ {
+		if in[i] == bound {
+			return in[:i]
+		}
+	}
+	return in
+}
+
+// generateSlice generates a new slice of len containing ascending sorted slice.
+func generateSlice(len int) []string {
+	if len <= 0 {
+		// minimum testable
+		// pageable amount
+		len = 2
+	}
+	now := time.Now()
+	in := make([]string, len)
+	for i := 0; i < len; i++ {
+		// Convert now to timestamp.
+		t := ulid.Timestamp(now)
+
+		// Create anew ulid for now.
+		u := ulid.MustNew(t, randRd)
+
+		// Add to slice.
+		in[i] = u.String()
+
+		// Bump now by 1 second.
+		now = now.Add(time.Second)
+	}
+	return in
 }
