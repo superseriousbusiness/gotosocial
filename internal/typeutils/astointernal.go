5452b4e283c675a7346f02fb4812188f17ea7e64
Paul Wilde <paul@paulwilde.uk>
2023-10-11 17:25:10 +0100

    go fmt'd

--- internal/typeutils/astointernal.go
+++ internal/typeutils/astointernal.go
@@ -147,9 +147,9 @@ func (c *Converter) ASRepresentationToAccount(ctx context.Context, accountable a
 	enableRSS := false
 	acct.EnableRSS = &enableRSS
 
-    // assume they don't want all replies shown on the home timeline
-    showAllReplies := false
-    acct.ShowAllReplies = &showAllReplies
+	// assume they don't want all replies shown on the home timeline
+	showAllReplies := false
+	acct.ShowAllReplies = &showAllReplies
 
 	// url property
 	url, err := ap.ExtractURL(accountable)
