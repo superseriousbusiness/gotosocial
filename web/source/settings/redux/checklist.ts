9275d791ed221f76388e80341509e5575aa409af
tsmethurst <tobi.smethurst@protonmail.com>
2023-10-21 14:57:14 +0200

    remove unused 'current' import

--- web/source/settings/redux/checklist.ts
+++ web/source/settings/redux/checklist.ts
@@ -17,7 +17,7 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { PayloadAction, createSlice, current } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import type { Checkable } from "../lib/form/types";
 import { useReducer } from "react";
 
