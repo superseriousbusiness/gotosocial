74ec3b85ef116f98e0617ffb624f22cf9e796245
tsmethurst <tobi.smethurst@protonmail.com>
2023-10-21 14:53:14 +0200

    found the bug!

--- web/source/settings/lib/form/check-list.tsx
+++ web/source/settings/lib/form/check-list.tsx
@@ -18,14 +18,11 @@
 */
 
 import {
-	useReducer,
 	useRef,
 	useEffect,
 	useCallback,
 	useMemo,
 } from "react";
-
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 import type {
 	Checkable,
@@ -34,106 +31,12 @@ import type {
 	HookOpts,
 } from "./types";
 
-// https://immerjs.github.io/immer/installation#pick-your-immer-version
-import { enableMapSet } from "immer";
-enableMapSet();
-
-interface ChecklistState {
-	entries: { [k: string]: Checkable },
-	selectedEntries: Set<string>,
-}
-
-const initialState: ChecklistState = {
-	entries: {},
-	selectedEntries: new Set(),
-};
-
-const { reducer, actions } = createSlice({
-	name: "checklist",
-	initialState, // not handled by slice itself
-	reducers: {
-		updateAll: (state, { payload: checked }: PayloadAction<boolean>) => {
-			const selectedEntries = new Set<string>();
-			const entries = Object.fromEntries(
-				Object.values(state.entries).map((entry) => {
-					if (checked) {
-						// Cheekily add this to selected
-						// entries while we're here.
-						selectedEntries.add(entry.key);
-					}
-
-					return [entry.key, { ...entry, checked } ];
-				})
-			);
-			
-			return { entries, selectedEntries };
-		},
-		update: (state, { payload: { key, value } }: PayloadAction<{key: string, value: Checkable}>) => {
-			if (value.checked !== undefined) {
-				if (value.checked === true) {
-					state.selectedEntries.add(key);
-				} else {
-					state.selectedEntries.delete(key);
-				}
-			}
-
-			state.entries[key] = {
-				...state.entries[key],
-				...value
-			};
-		},
-		updateMultiple: (state, { payload }: PayloadAction<Array<[key: string, value: Checkable]>>) => {
-			payload.forEach(([key, value]) => {
-				if (value.checked !== undefined) {
-					if (value.checked === true) {
-						state.selectedEntries.add(key);
-					} else {
-						state.selectedEntries.delete(key);
-					}
-				}
-
-				state.entries[key] = {
-					...state.entries[key],
-					...value
-				};
-			});
-		}
-	}
-});
-
-function initialHookState({
-	entries,
-	uniqueKey,
-	initialValue,
-}: {
-	entries: Checkable[],
-	uniqueKey: string,
-	initialValue: boolean,
-}): ChecklistState {
-	const selectedEntries = new Set<string>();
-	const mappedEntries = Object.fromEntries(
-		entries.map((entry) => {
-			const key = entry[uniqueKey];
-			const checked = entry.checked ?? initialValue;
-			
-			if (checked) {
-				selectedEntries.add(key);
-			} else {
-				selectedEntries.delete(key);
-			}
-		
-			return [ key, { ...entry, key, checked } ];
-		})
-	);
-
-	return {
-		entries: mappedEntries,
-		selectedEntries
-	};
-}
+import {
+	useChecklistReducer,
+	actions,
+} from "../../redux/checklist";
 
 const _default: { [k: string]: Checkable } = {};
-
 export default function useCheckListInput(
 	/* eslint-disable no-unused-vars */
 	{ name, Name }: CreateHookNames,
@@ -143,12 +46,7 @@ export default function useCheckListInput(
 		initialValue = false,
 	}: HookOpts<boolean>
 ): ChecklistInputHook {
-	const [state, dispatch] = useReducer(
-		reducer,
-		initialState,
-		(_) => initialHookState({ entries, uniqueKey, initialValue }) // initial state
-	);
-
+	const [state, dispatch] = useChecklistReducer(entries, uniqueKey, initialValue);
 	const toggleAllRef = useRef<any>(null);
 
 	useEffect(() => {
@@ -171,12 +69,12 @@ export default function useCheckListInput(
 	);
 
 	const onChange = useCallback(
-		(key, value) => dispatch(actions.update({ key, value })),
+		(key: string, value: Checkable) => dispatch(actions.update({ key, value })),
 		[]
 	);
 
 	const updateMultiple = useCallback(
-		(entries) => dispatch(actions.updateMultiple(entries)),
+		(entries: [key: string, value: Partial<Checkable>][]) => dispatch(actions.updateMultiple(entries)),
 		[]
 	);
 
---
+++ web/source/settings/redux/checklist.ts
@@ -0,0 +1,130 @@
+/*
+	GoToSocial
+	Copyright (C) GoToSocial Authors admin@gotosocial.org
+	SPDX-License-Identifier: AGPL-3.0-or-later
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { PayloadAction, createSlice, current } from "@reduxjs/toolkit";
+import type { Checkable } from "../lib/form/types";
+import { useReducer } from "react";
+
+// https://immerjs.github.io/immer/installation#pick-your-immer-version
+import { enableMapSet } from "immer";
+enableMapSet();
+
+export interface ChecklistState {
+	entries: { [k: string]: Checkable },
+	selectedEntries: Set<string>,
+}
+
+const initialState: ChecklistState = {
+	entries: {},
+	selectedEntries: new Set(),
+};
+
+function initialHookState({
+	entries,
+	uniqueKey,
+	initialValue,
+}: {
+	entries: Checkable[],
+	uniqueKey: string,
+	initialValue: boolean,
+}): ChecklistState {
+	const selectedEntries = new Set<string>();
+	const mappedEntries = Object.fromEntries(
+		entries.map((entry) => {
+			const key = entry[uniqueKey];
+			const checked = entry.checked ?? initialValue;
+			
+			if (checked) {
+				selectedEntries.add(key);
+			} else {
+				selectedEntries.delete(key);
+			}
+		
+			return [ key, { ...entry, key, checked } ];
+		})
+	);
+
+	return {
+		entries: mappedEntries,
+		selectedEntries
+	};
+}
+
+const checklistSlice = createSlice({
+	name: "checklist",
+	initialState, // not handled by slice itself
+	reducers: {
+		updateAll: (state, { payload: checked }: PayloadAction<boolean>) => {
+			const selectedEntries = new Set<string>();
+			const entries = Object.fromEntries(
+				Object.values(state.entries).map((entry) => {
+					if (checked) {
+						// Cheekily add this to selected
+						// entries while we're here.
+						selectedEntries.add(entry.key);
+					}
+
+					return [entry.key, { ...entry, checked } ];
+				})
+			);
+			
+			return { entries, selectedEntries };
+		},
+		update: (state, { payload: { key, value } }: PayloadAction<{key: string, value: Partial<Checkable>}>) => {
+			if (value.checked !== undefined) {
+				if (value.checked) {
+					state.selectedEntries.add(key);
+				} else {
+					state.selectedEntries.delete(key);
+				}
+			}
+
+			state.entries[key] = {
+				...state.entries[key],
+				...value
+			};
+		},
+		updateMultiple: (state, { payload }: PayloadAction<Array<[key: string, value: Partial<Checkable>]>>) => {						
+			payload.forEach(([ key, value ]) => {								
+				if (value.checked !== undefined) {
+					if (value.checked) {
+						state.selectedEntries.add(key);
+					} else {
+						state.selectedEntries.delete(key);
+					}
+				}
+
+				state.entries[key] = {
+					...state.entries[key],
+					...value
+				};
+			});
+		}
+	}
+});
+
+export const actions = checklistSlice.actions;
+
+export const useChecklistReducer = (entries: Checkable[], uniqueKey: string, initialValue: boolean) => {
+	return useReducer(
+		checklistSlice.reducer,
+		initialState,
+		(_) => initialHookState({ entries, uniqueKey, initialValue })
+	);
+}
