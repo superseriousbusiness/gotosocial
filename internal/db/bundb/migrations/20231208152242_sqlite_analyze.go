7881f7af2ea7b4c56eb0b91348d1789cff696d70
tobi <tobi.smethurst@protonmail.com>
2023-12-08 16:39:54 +0100

    run ANALYZE

---
+++ internal/db/bundb/migrations/20231208152242_sqlite_analyze.go
@@ -0,0 +1,51 @@
+// GoToSocial
+// Copyright (C) GoToSocial Authors admin@gotosocial.org
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package migrations
+
+import (
+	"context"
+
+	"github.com/superseriousbusiness/gotosocial/internal/log"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect"
+)
+
+func init() {
+	up := func(ctx context.Context, db *bun.DB) error {
+		if db.Dialect().Name() != dialect.SQLite {
+			// No need to run this on
+			// other types than SQLite.
+			return nil
+		}
+
+		log.Info(ctx, "running ANALYZE on SQLite database for optimization; this may take some time, please be patient and don't interrupt this!")
+		if _, err := db.ExecContext(ctx, "ANALYZE"); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	down := func(ctx context.Context, db *bun.DB) error {
+		return nil
+	}
+
+	if err := Migrations.Register(up, down); err != nil {
+		panic(err)
+	}
+}
