9607b482cc68d24ef700b4777c557b33f84426c0
tobi <31960611+tsmethurst@users.noreply.github.com>
2024-01-13 16:33:53 +0100

    [chore/docs] Replace specific year range of copyright notice (#2520)

--- web/source/css/tag.css
+++ web/source/css/tag.css
@@ -1,6 +1,7 @@
 /*
 	GoToSocial
-	Copyright (C) 2021-2023 GoToSocial Authors admin@gotosocial.org
+	Copyright (C) GoToSocial Authors admin@gotosocial.org
+	SPDX-License-Identifier: AGPL-3.0-or-later
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published by
