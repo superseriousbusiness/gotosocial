52330d0481c657d339432ab693ff701f2530f3a5
Vyr Cossont <VyrCossont@users.noreply.github.com>
2024-02-29 14:44:13 -0800

    Remove Swagger prefix from dummy fields
    It makes the generated code look weird

--- internal/api/model/status.go
+++ internal/api/model/status.go
@@ -179,26 +179,26 @@ type StatusCreateRequest struct {
 	// in: formData
 	MediaIDs []string `form:"media_ids[]" json:"media_ids" xml:"media_ids"`
 	// Poll to include with this status.
-	// The SwaggerPoll… parameters should only be used to document this complex parameter,
+	// The Poll… parameters below only exist to document this struct parameter for Swagger,
 	// and should not be used otherwise.
 	// swagger:ignore
 	Poll *PollRequest `form:"poll" json:"poll" xml:"poll"`
 	// Array of possible answers.
 	// If provided, media_ids cannot be used, and poll[expires_in] must be provided.
 	// in: formData
-	SwaggerPollOptions []string `form:"-" json:"poll[options][]" xml:"-"`
+	PollOptions []string `form:"-" json:"poll[options][]" xml:"-"`
 	// Duration the poll should be open, in seconds.
 	// If provided, media_ids cannot be used, and poll[options] must be provided.
 	// in: formData
-	SwaggerPollExpiresIn int `form:"-" json:"poll[expires_in]" xml:"-"`
+	PollExpiresIn int `form:"-" json:"poll[expires_in]" xml:"-"`
 	// Allow multiple choices on this poll.
 	// default: false
 	// in: formData
-	SwaggerPollMultiple bool `form:"-" json:"poll[multiple]" xml:"-"`
+	PollMultiple bool `form:"-" json:"poll[multiple]" xml:"-"`
 	// Hide vote counts until the poll ends.
 	// default: true
 	// in: formData
-	SwaggerPollHideTotals bool `form:"-" json:"poll[hide_totals]" xml:"-"`
+	PollHideTotals bool `form:"-" json:"poll[hide_totals]" xml:"-"`
 	// ID of the status being replied to, if status is a reply.
 	// in: formData
 	InReplyToID string `form:"in_reply_to_id" json:"in_reply_to_id" xml:"in_reply_to_id"`
