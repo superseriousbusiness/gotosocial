fdefe72709c2b7ff32c37c2a6f84b8e5b38dea99
tobi <tobi.smethurst@protonmail.com>
2024-03-01 12:00:03 +0100

    update comment

--- internal/gtsmodel/account.go
+++ internal/gtsmodel/account.go
@@ -55,7 +55,7 @@ type Account struct {
 	Memorial                *bool            `bun:",default:false"`                 // Is this a memorial account, ie., has the user passed away?
 	AlsoKnownAsURIs         []string         `bun:"also_known_as_uris,array"`       // This account is associated with these account URIs.
 	AlsoKnownAs             []*Account       `bun:"-"`                              // This account is associated with these accounts (field not stored in the db).
-	MovedToURI              string           `bun:",nullzero"`                      // This account has moved to this account URI. Even if this field is set the move may not yet have been processed. Check `moved_at` for this.
+	MovedToURI              string           `bun:",nullzero"`                      // This account has (or claims to have) moved to this account URI. Even if this field is set the move may not yet have been processed. Check `move` for this.
 	MovedTo                 *Account         `bun:"-"`                              // This account has moved to this account (field not stored in the db).
 	MoveID                  string           `bun:""`                               // ID of a Move in the database for this account. Only set if we received or created a Move activity for which this account URI was the origin.
 	Move                    *Move            `bun:"-"`                              // Move corresponding to MoveID, if set.
