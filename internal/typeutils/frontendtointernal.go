7ebbbb4063c552d5002b231777c0951c96928728
Vyr Cossont <VyrCossont@users.noreply.github.com>
2024-03-31 23:44:18 -0700

    APIFilterActionToFilterAction not used yet

--- internal/typeutils/frontendtointernal.go
+++ internal/typeutils/frontendtointernal.go
@@ -47,13 +47,3 @@ func APIMarkerNameToMarkerName(m apimodel.MarkerName) gtsmodel.MarkerName {
 	}
 	return ""
 }
-
-func APIFilterActionToFilterAction(m apimodel.FilterAction) gtsmodel.FilterAction {
-	switch m {
-	case apimodel.FilterActionWarn:
-		return gtsmodel.FilterActionWarn
-	case apimodel.FilterActionHide:
-		return gtsmodel.FilterActionHide
-	}
-	return ""
-}
