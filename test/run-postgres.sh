2bfa1fcf2f4b13f5868219eb7f94374633610eb2
kim <grufwub@gmail.com>
2024-04-23 14:25:38 +0100

    make run-postgres compatible with podman

--- test/run-postgres.sh
+++ test/run-postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 # Determine available docker binary
 _docker=$(command -v 'podman') || \
@@ -15,10 +15,12 @@ ARGS='./...'
 DB_NAME='postgres'
 DB_USER='postgres'
 DB_PASS='postgres'
+DB_IP='127.0.0.1'
 DB_PORT=5432
 
 # Start postgres container
 CID=$($_docker run --detach \
+    --publish "${DB_IP}:${DB_PORT}:${DB_PORT}" \
     --env "POSTGRES_DB=${DB_NAME}" \
     --env "POSTGRES_USER=${DB_USER}" \
     --env "POSTGRES_PASSWORD=${DB_PASS}" \
@@ -34,14 +36,11 @@ sleep 5
 #docker exec "$CID" psql --user "$DB_USER" --password "$DB_PASS" -c "CREATE DATABASE \"${DB_NAME}\" WITH LOCALE \"C.UTF-8\" TEMPLATE \"template0\";"
 $_docker exec "$CID" psql --user "$DB_USER" --password "$DB_PASS" -c "GRANT ALL PRIVILEGES ON DATABASE \"${DB_NAME}\" TO \"${DB_USER}\";"
 
-# Get running container IP
-IP=$($_docker container inspect "${CID}" \
-    --format '{{ .NetworkSettings.IPAddress }}')
-
+env \
 GTS_DB_TYPE=postgres \
-GTS_DB_ADDRESS=${IP} \
+GTS_DB_ADDRESS=${DB_IP} \
 GTS_DB_PORT=${DB_PORT} \
 GTS_DB_USER=${DB_USER} \
 GTS_DB_PASSWORD=${DB_PASS} \
 GTS_DB_DATABASE=${DB_NAME} \
-go test ./... -p 1 ${ARGS}
+go test -p 1 ${ARGS}
