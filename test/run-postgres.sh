3b62c6be515050f8098bfa3032e44140b6bca8d5
kim <grufwub@gmail.com>
2024-04-23 13:28:00 +0100

    update postgres test script to support docker or podman

--- test/run-postgres.sh
+++ test/run-postgres.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# Determine available docker binary
+_docker=$(command -v 'podman') || \
+_docker=$(command -v 'docker') || \
+{ echo 'docker not found'; exit 1; }
+
 # Ensure test args are set.
 ARGS=${@}; [ -z "$ARGS" ] && \
 ARGS='./...'
@@ -13,24 +18,24 @@ DB_PASS='postgres'
 DB_PORT=5432
 
 # Start postgres container
-CID=$(docker run --detach \
+CID=$($_docker run --detach \
     --env "POSTGRES_DB=${DB_NAME}" \
     --env "POSTGRES_USER=${DB_USER}" \
     --env "POSTGRES_PASSWORD=${DB_PASS}" \
     --env "POSTGRES_HOST_AUTH_METHOD=trust" \
     --env "PGHOST=0.0.0.0" \
     --env "PGPORT=${DB_PORT}" \
-    'postgres:latest')
+    'docker.io/postgres:latest')
 
 # On exit kill the container
-trap "docker kill ${CID}" exit
+trap "$_docker kill ${CID}" exit
 
 sleep 5
 #docker exec "$CID" psql --user "$DB_USER" --password "$DB_PASS" -c "CREATE DATABASE \"${DB_NAME}\" WITH LOCALE \"C.UTF-8\" TEMPLATE \"template0\";"
-docker exec "$CID" psql --user "$DB_USER" --password "$DB_PASS" -c "GRANT ALL PRIVILEGES ON DATABASE \"${DB_NAME}\" TO \"${DB_USER}\";"
+$_docker exec "$CID" psql --user "$DB_USER" --password "$DB_PASS" -c "GRANT ALL PRIVILEGES ON DATABASE \"${DB_NAME}\" TO \"${DB_USER}\";"
 
 # Get running container IP
-IP=$(docker container inspect "${CID}" \
+IP=$($_docker container inspect "${CID}" \
     --format '{{ .NetworkSettings.IPAddress }}')
 
 GTS_DB_TYPE=postgres \
