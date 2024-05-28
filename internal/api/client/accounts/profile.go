134ad84f255230e6c103fbc035728ecadffc962a
Vyr Cossont <VyrCossont@users.noreply.github.com>
2024-05-28 15:34:15 -0700

    Remove check for moved account

--- internal/api/client/accounts/profile.go
+++ internal/api/client/accounts/profile.go
@@ -108,11 +108,6 @@ func (m *Module) accountDeleteProfileAttachment(c *gin.Context, processDelete fu
 		return
 	}
 
-	if authed.Account.IsMoving() {
-		apiutil.ForbiddenAfterMove(c)
-		return
-	}
-
 	if _, err := apiutil.NegotiateAccept(c, apiutil.JSONAcceptHeaders...); err != nil {
 		apiutil.ErrorHandler(c, gtserror.NewErrorNotAcceptable(err, err.Error()), m.processor.InstanceGetV1)
 		return
