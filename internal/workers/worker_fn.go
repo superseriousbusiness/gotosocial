452c6c3e3fd4983d6f6034fd9205fd56ad19d164
Daenney <daenney@users.noreply.github.com>
2024-05-29 23:50:56 +0200

    [chore] Make worker run messages debug output
    On startup and shutdown of a worker, we log a message of the worker
    being started together with a textual representation of a memory
    address. Though this can be handy for developers to debug
    startup/shutdown sequencing issues of the workers, it's typically not
    very useful or informative for an admin. We can also output a lot of
    these (on my system I get 265 lines of these during startup).
    
    This changes the messages from Info to Debug, to not print them under
    normal circumstances.

--- internal/workers/worker_fn.go
+++ internal/workers/worker_fn.go
@@ -114,8 +114,8 @@ func (w *FnWorker) run(ctx context.Context) {
 	if w.Queue == nil {
 		panic("not yet initialized")
 	}
-	log.Infof(ctx, "%p: starting worker", w)
-	defer log.Infof(ctx, "%p: stopped worker", w)
+	log.Debugf(ctx, "%p: starting worker", w)
+	defer log.Debugf(ctx, "%p: stopped worker", w)
 	util.Must(func() { w.process(ctx) })
 }
 
