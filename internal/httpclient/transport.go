d0112b27b5d9dac22e6f097a1c3d24440b060af3
kim <grufwub@gmail.com>
2024-06-07 13:16:03 +0100

    standard library already handles rewinding

--- internal/httpclient/transport.go
+++ internal/httpclient/transport.go
@@ -37,11 +37,6 @@ func (t *signingtransport) RoundTrip(r *http.Request) (*http.Response, error) {
 	// Ensure updated host always set.
 	r.Header.Set("Host", r.URL.Host)
 
-	// // Rewind request body if needed.
-	// if err := rewind(r); err != nil {
-	// 	return nil, err
-	// }
-
 	if sign := gtscontext.HTTPClientSignFunc(r.Context()); sign != nil {
 		// Reset signing header fields
 		now := time.Now().UTC()
@@ -57,48 +52,4 @@ func (t *signingtransport) RoundTrip(r *http.Request) (*http.Response, error) {
 
 	// Pass to underlying transport.
 	return t.Transport.RoundTrip(r)
-}
-
-// rewind attempts to rewind a request body
-// if required, i.e. starts it from beginning.
-func rewind(r *http.Request) error {
-	// No rewind needed.
-	if r.Body == nil {
-		return nil
-	}
-
-	// Check for size support.
-	szer, ok := r.Body.(sizer)
-
-	// Both sizing and body func
-	// are required for rewinding.
-	if !ok || r.GetBody == nil {
-		return nil
-	}
-
-	// Check whether body is at start of in data.
-	if s := szer.Size(); s != int64(szer.Len()) {
-		var err error
-
-		// Get new copy of body.
-		r.Body, err = r.GetBody()
-		if err != nil {
-			return err
-		}
-
-		// Reset content-length.
-		r.ContentLength = s
-	}
-
-	return nil
-}
-
-// sizer is an interface supported by
-// common stdlib implementers of io
-// interfaces. e.g. strings.Reader,
-// bytes.Reader (the only types we
-// use with known rewindable bodies).
-type sizer interface {
-	Len() int
-	Size() int64
 }
