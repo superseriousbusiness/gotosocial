8aa8e0127f6c8d34bcbc53016fb4802506f17e06
kim <grufwub@gmail.com>
2024-08-16 19:42:29 +0100

    add note about copying from github.com/disintegration/imaging

--- internal/media/probe.go
+++ internal/media/probe.go
@@ -1,3 +1,20 @@
+// GoToSocial
+// Copyright (C) GoToSocial Authors admin@gotosocial.org
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package media
 
 import (
@@ -103,6 +120,9 @@ func probeJPEG(file *os.File) (*result, error) {
 
 // readOrientation reads orientation EXIF
 // data (if it even exists) from image file.
+//
+// copied from github.com/disintegration/imaging
+// but modified to optimize discard operations.
 func readOrientation(r *os.File) int {
 	const (
 		markerAPP1     = 0xffe1
