9b2f14b13149546de909b4489d19ef8cf611fd59
tobi <31960611+tsmethurst@users.noreply.github.com>
2024-08-20 18:56:42 +0200

    [bugfix/frontend] Fix error on submitting domain perm with enter key (#3218)

--- web/source/settings/views/moderation/domain-permissions/overview.tsx
+++ web/source/settings/views/moderation/domain-permissions/overview.tsx
@@ -100,7 +100,7 @@ function DomainPermsList({ data, permType, permTypeUpper }: DomainPermsListProps
 	
 	function filterFormSubmit(e) {
 		e.preventDefault();
-		setLocation(`/${filter}`);
+		setLocation(`/${permType}s/${filter}`);
 	}
 	
 	const filter = filterField.value ?? "";
@@ -136,12 +136,15 @@ function DomainPermsList({ data, permType, permTypeUpper }: DomainPermsListProps
 					placeholder="example.org"
 					label={`Search or add domain ${permType}`}
 				/>
-				<Link
-					className="button"
-					to={`/${permType}s/${filter}`}
+				<button
+					type="submit"
+					disabled={
+						filterField.value === undefined ||
+						filterField.value.length == 0
+					}
 				>
 					{permTypeUpper}&nbsp;{filter}
-				</Link>
+				</button>
 			</form>
 			<div>
 				{filterInfo}
