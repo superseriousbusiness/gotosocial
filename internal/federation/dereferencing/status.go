d6d1d4d493438f480d57fa60e40b52b7814f2754
tobi <tobi.smethurst@protonmail.com>
2024-08-25 11:58:14 +0200

    comment

--- internal/federation/dereferencing/status.go
+++ internal/federation/dereferencing/status.go
@@ -520,6 +520,9 @@ func (d *Dereferencer) enrichStatus(
 	// based on the author's inclusion in a followers/following
 	// collection. By carrying over previously-set values we
 	// can avoid marking such statuses as "pending" again.
+	//
+	// If a remote has in the meantime retracted its approval,
+	// the next call to 'isPermittedStatus' will catch that.
 	latestStatus.ApprovedByURI = status.ApprovedByURI
 
 	// Check if this is a permitted status we should accept.
