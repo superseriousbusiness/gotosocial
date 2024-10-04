b35a36602d30d9c8615959112d8ed736863d241a
tobi <tobi.smethurst@protonmail.com>
2024-10-04 19:10:53 +0200

    [bugfix] Account for nil reply when serializing int req

--- internal/typeutils/internaltofrontend.go
+++ internal/typeutils/internaltofrontend.go
@@ -2680,7 +2680,7 @@ func (c *Converter) InteractionReqToAPIInteractionReq(
 	}
 
 	var reply *apimodel.Status
-	if req.InteractionType == gtsmodel.InteractionReply {
+	if req.InteractionType == gtsmodel.InteractionReply && req.Reply != nil {
 		reply, err = c.statusToAPIStatus(
 			ctx,
 			req.Reply,
