2aee3a260586b018af29733c646d508302877991
kim <grufwub@gmail.com>
2025-01-15 16:00:25 +0000

    keep a cache of activity IDs we have handled creates for

--- internal/federation/federatingdb/create.go
+++ internal/federation/federatingdb/create.go
@@ -63,6 +63,10 @@ func (f *federatingDB) Create(ctx context.Context, asType vocab.Type) error {
 		return nil
 	}
 
+	// Cache entry for this create activity ID for later
+	// checks in the Exist() function if we see it again.
+	f.createdIDs.Set(ap.GetJSONLDId(asType).String(), struct{}{})
+
 	switch name := asType.GetTypeName(); name {
 	case ap.ActivityBlock:
 		// BLOCK SOMETHING
