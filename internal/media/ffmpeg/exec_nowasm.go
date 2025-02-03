3932f9d52b1e57bfec1c57166a6484f56d530cd3
kim <grufwub@gmail.com>
2025-02-03 12:06:30 +0000

    adds support for build specifically without wasm ffmpeg

--- internal/media/ffmpeg/exec_nowasm.go
+++ internal/media/ffmpeg/exec_nowasm.go
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-//go:build nowasm
+//go:build nowasmffmpeg || nowasm
 
 package ffmpeg
 
