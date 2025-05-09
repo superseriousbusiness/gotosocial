8c1511a494c1af8ded1f246c865c648fd97bae43
tobi <tobi.smethurst@protonmail.com>
2025-05-09 16:14:10 +0000

    [chore] Poke at `bundle_licenses.sh` a bit to make the output slightly easier on the eye (#4159)
    Reviewed-on: https://codeberg.org/superseriousbusiness/gotosocial/pulls/4159
    Co-authored-by: tobi <tobi.smethurst@protonmail.com>
    Co-committed-by: tobi <tobi.smethurst@protonmail.com>

--- scripts/bundle_licenses.sh
+++ scripts/bundle_licenses.sh
@@ -9,26 +9,36 @@ IFS='
 license_filter() { grep -iE '.*/license(\.\w+)?$'; }
 
 OUTPUT='./web/assets/all_licenses.txt'
-echo '' > "$OUTPUT" # reset output
+echo 'The GoToSocial software uses the following dependencies, whose licenses are reproduced in full:' > "$OUTPUT" # reset output
+echo >> "$OUTPUT"
 
 # Copy over any licenses in our golang dependencies
 for file in $(find ./vendor | license_filter); do
-    echo "${file}:" >> "$OUTPUT"
-    cat  "$file"    >> "$OUTPUT"
-    echo            >> "$OUTPUT"
+    echo "----------------------------------------------------------" >> "$OUTPUT"
+    echo                                                              >> "$OUTPUT"
+    echo "${file}:"                                                   >> "$OUTPUT"
+    echo                                                              >> "$OUTPUT"
+    cat  "${file}"                                                    >> "$OUTPUT"
+    echo                                                              >> "$OUTPUT"
 done
 
 # Copy over any licenses in our javacsript dependencies
 for file in $(find ./web/source | license_filter); do
-    echo "${file}:" >> "$OUTPUT"
-    cat  "$file"    >> "$OUTPUT"
-    echo            >> "$OUTPUT"
+    echo "----------------------------------------------------------" >> "$OUTPUT"
+    echo                                                              >> "$OUTPUT"
+    echo "${file}:"                                                   >> "$OUTPUT"
+    echo                                                              >> "$OUTPUT"
+    cat  "${file}"                                                    >> "$OUTPUT"
+    echo                                                              >> "$OUTPUT"
 done
 
 # Copy over misc other licenses
 for file in ./LICENSE \
             ./web/source/nollamasworker/sha256.js; do
-    echo "${file}:" >> "$OUTPUT"
-    cat  "$file"    >> "$OUTPUT"
-    echo            >> "$OUTPUT"
+    echo "----------------------------------------------------------" >> "$OUTPUT"
+    echo                                                              >> "$OUTPUT"
+    echo "${file}:"                                                   >> "$OUTPUT"
+    echo                                                              >> "$OUTPUT"
+    cat  "${file}"                                                    >> "$OUTPUT"
+    echo                                                              >> "$OUTPUT"
 done
